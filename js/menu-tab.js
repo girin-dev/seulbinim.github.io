65da0aef4bc6e1aac56c8f1171ac40ed8577781d
kw8799 <kw8799@gmail.com>
2017-09-18 17:31:32 +0900

    add css/stylesheet.css js/menu-tab.js main.html make group2 notice pds news

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var span = $('.main-menu span');
   var last_item = $('.sub-menu li:last-child a');
 
+
   menu.hover(function() {
     $(this).find('.sub-menu').toggleClass('sub-menu-active');
   });
@@ -14,4 +15,38 @@ $(document).ready(function() {
   last_item.focusout(function() {
     $(this).parents(".sub-menu").removeClass('sub-menu-active');
   });
+
+  var tab = $('.tab');
+
+  // tab.click(function() {
+  //   $(this).parent().addClass('board-active').siblings().removeClass('board-active');
+  // });
+  // tab.focusin(function() {
+  //   $(this).parent().addClass('board-active').siblings().removeClass('board-active');
+  // });
+  tab.on('click focusin', function() {
+    $(this).parent().addClass('board-active').siblings().removeClass('board-active');
+  });
+
+  // 내가 짠 코드
+  // var notice = $('.notice-heading');
+  // var pds = $('.pds-heading')
+
+  // 내가 짠 코드
+  // notice.focusin(function() {
+  //   $(this).parents('.notice').addClass('board-active')
+  // });
+
+  // notice.focusout(function() {
+  //   $(this).parents('.notice').removeClass('board-active')
+  // });
+
+  // pds.focusin(function() {
+  //   $(this).parents('.pds').addClass('board-active')
+  // });
+
+  // pds.focusout(function() {
+  //   $(this).parents('.pds').removeClass('board-active')
+  // });
+
 });
